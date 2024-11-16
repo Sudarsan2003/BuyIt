a698db85d051fa940acbcd31bb5c0905b90d445a
SudarsanAnnaluru <annalurusudarsan@gmail.com>
2024-11-16 17:41:26 +0530

    Updated Home page layout and added NewComponent

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,43 +1,53 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import emailjs from 'emailjs-com';  
+import emailjs from 'emailjs-com';
+import Modal from '../components/Modal.js'; 
 import '../contactform.css';
 
 function Contact() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
-    message: ''
+    message: '',
   });
 
-  const [status, setStatus] = useState(""); 
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-   
-    emailjs.sendForm(
-      'service_a9jl199',    
-      'template_dfuqm7n',  
-      e.target,            
-      'EUE2coetnuOQWq8KW'  
-    ).then((result) => {
-      console.log('Success:', result.text);
-      setStatus("Message sent successfully!");
-      navigate('/'); 
-    }).catch((error) => {
-      console.log('Error:', error); 
-      setStatus("Failed to send message. Please try again later.");
-    });
-    
+
+    emailjs
+      .sendForm(
+        'service_a9jl199', 
+        'template_dfuqm7n', 
+        e.target,
+        'EUE2coetnuOQWq8KW' 
+      )
+      .then(() => {
+        setShowModal(true); 
+      })
+      .catch(() => {
+        alert('Failed to send message. Please try again later.');
+      });
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    navigate('/');
+  };
+
+  const handleSaveChanges = () => {
+    alert('Changes saved!');
+    handleCloseModal();
   };
 
   return (
@@ -45,40 +55,43 @@ function Contact() {
       <h2>Contact Us</h2>
       <form onSubmit={handleSubmit} method="POST" className="contact-form">
         <label htmlFor="name">Your Name</label>
-        <input 
-          type="text" 
-          id="name" 
-          name="name" 
+        <input
+          type="text"
+          id="name"
+          name="name"
           value={formData.name}
           onChange={handleInputChange}
-          required 
+          required
         />
         <label htmlFor="email">Your Email</label>
-        <input 
-          type="email" 
-          id="email" 
-          name="email" 
+        <input
+          type="email"
+          id="email"
+          name="email"
           value={formData.email}
           onChange={handleInputChange}
-          required 
+          required
         />
         <label htmlFor="message">Your Message</label>
-        <textarea 
-          id="message" 
-          name="message" 
-          rows="4" 
+        <textarea
+          id="message"
+          name="message"
+          rows="4"
           value={formData.message}
           onChange={handleInputChange}
           required
         ></textarea>
-
         <button type="submit" className="submit-btn">Submit</button>
       </form>
-      <p className={status.includes("successfully") ? "status-success" : "status-error"}>
-  {status}
-</p>
 
-      
+      {showModal && (
+        <Modal
+          title="Thank You!"
+          body="We received your feedback. We will get back to you soon... 😊"
+          onClose={handleCloseModal}
+          onSave={handleSaveChanges}
+        />
+      )}
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,107 @@
-import React from 'react'
+import React from 'react';
+import '../home.css';
+
 function Home() {
-    
-    return (
-<div id="home-container">
+  return (
+    <>
+    <div className="banner">
+      <div
+        className="slider"
+        style={{
+          '--quantity': 10, 
+        }}
+      >
+        <div
+          className="item"
+          style={{
+            '--position': 1,
+          }}
+        >
+          <img src="https://boldoutline.in/wp-content/uploads/2019/10/Web-Cover-82.jpg" alt="Dragon 1" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 2,
+          }}
+        >
+          <img src="https://thumbs.dreamstime.com/b/concept-grocery-shopping-home-delivery-app-filled-package-goods-decoration-wallpaper-desktop-poster-booklet-cover-298213803.jpg" alt="Dragon 2" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 3,
+          }}
+        >
+          <img src="https://wallpapers.com/images/hd/jewellery-background-2880-x-1800-dgixvilq78viyf9w.jpg" alt="Dragon 3" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 4,
+          }}
+        >
+          <img src="https://t4.ftcdn.net/jpg/03/64/41/07/360_F_364410756_Ev3WoDfNyxO9c9n4tYIsU5YBQWAP3UF8.jpg" alt="Dragon 4" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 5,
+          }}
+        >
+          <img src="https://images.pexels.com/photos/297933/pexels-photo-297933.jpeg?cs=srgb&dl=pexels-david-bartus-43782-297933.jpg&fm=jpg" alt="Dragon 5" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 6,
+          }}
+        >
+          <img src="https://media.istockphoto.com/id/1256474013/photo/hanger-with-pink-womens-clothing-against-the-background-of-a-pink-wall-monotonous-pink.jpg?s=612x612&w=0&k=20&c=Cuq2ybBIJLtRLI_DS81mwF8oCFmDBJZDQ6KnadY3ZO0=" alt="Dragon 6" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 7,
+          }}
+        >
+          <img src="https://www.hdwallpapers.in/download/cute_babies_lying_in_green_carpet_wearing_cartoon_dress_4k_5k_hd_cute-5120x2880.jpg" alt="Dragon 7" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 8,
+          }}
+        >
+          <img src="https://i.pinimg.com/550x/85/8e/6c/858e6c988cf463dc3059e872339e4dae.jpg" alt="Dragon 8" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 9,
+          }}
+        >
+          <img src="https://5.imimg.com/data5/XM/TS/HL/GLADMIN-81686742/086-500x500.png" alt="Dragon 9" />
+        </div>
+        <div
+          className="item"
+          style={{
+            '--position': 10,
+          }}
+        >
+          <img src="https://redtape.com/cdn/shop/files/RSO4065_1.jpg?v=1711715382" alt="Dragon 10" />
+        </div>
+      </div>
+      <div className="content">
+        <h1 data-content="BUYIT">BuyIt</h1>
+        <div className="author">
+        </div>
+        <div className="model"></div>
+      </div>
+    </div>
+    <div className="white-section">
+        <h2>Explore Our Categories</h2>
+        <div className="container">
   <div className="landing-cards" id="card-1">
     <div className="card-content">
       <p className="card-text">Cosmetics</p>
@@ -20,7 +119,7 @@ function Home() {
   </div>
   <div className="landing-cards" id="card-4">
     <div className="card-content">
-      <p className="card-text">E-Gadjets</p>
+      <p className="card-text">E-Gadgets</p>
     </div>
   </div>
   <div className="landing-cards" id="card-5">
@@ -33,9 +132,31 @@ function Home() {
       <p className="card-text">Women Wear</p>
     </div>
   </div>
+  <div className="landing-cards" id="card-7">
+    <div className="card-content">
+      <p className="card-text">Kid's Wear</p>
+    </div>
+  </div>
+  <div className="landing-cards" id="card-8">
+    <div className="card-content">
+      <p className="card-text">Party Wear</p>
+    </div>
+  </div>
+  <div className="landing-cards" id="card-9">
+    <div className="card-content">
+      <p className="card-text">Watches</p>
+    </div>
+  </div>
+  <div className="landing-cards" id="card-10">
+    <div className="card-content">
+      <p className="card-text">Shoes</p>
+    </div>
+  </div>
 </div>
 
-    )
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
